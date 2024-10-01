c2ee00bb55145b894c476c79e9ca2a6c7e12ea2e
ttluser <ttluser@INHNJLPT-975968.tatatechnologies.com>
2024-10-02 00:40:19 +0530

    firstcommit

---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,16 @@
+import java.sql.SQLOutput;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        int a = 10;
+        int b = 30;
+        double c = 5.5;
+        String s = "This is for string concepts";
+        System.out.println("Result: " + a / b + 20);
+        System.out.println("Value for s is " + s.isBlank());
+
+
+
+    }
+}
